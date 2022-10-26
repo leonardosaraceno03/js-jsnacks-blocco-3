0055e019a403527ee9a10a948999c4945f4e0d9e
LeonardoSaraceno <leonardosaraceno03@gmail.com>
2022-10-26 17:59:45 +0200

    snack 1 ottimizzato

--- snack 1/main.js
+++ snack 1/main.js
@@ -6,16 +6,15 @@ let array = [];
 let somma = 0;
 
 let numero = 0;
-
+// ciclo per inserire numeri nell'array
 while (somma < 50){
     numero = parseInt(prompt("inserisci un numero"));
     somma += numero;
-    if(somma < 50){
-        array.push(numero)
-    }
+    array.push(numero)
+    
 }
 
-
+// stampa somma < 50
 let lastItem=array[array.length-1];
  somma = somma - lastItem;
  console.log(somma)
